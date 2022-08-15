0b0f72d7586a8beaeb7aaac83ba226e9c7f18fdd
Nuckle <37220313+nuckle@users.noreply.github.com>
2022-08-15 23:10:34 +0300

    Updated

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '57a81eaa'
+version = '0.1.0+dev.57a81eaa'
+linkified_version = '0.1.0+dev.[57a81eaa](https://github.com/maunium/stickerpicker/commit/57a81eaab44e2a1235bca569f1f1dcb2485bd1db)'
